dc09d8ab2febabf05a564bad6e8223434f41bc0e
ting <ttzhenng@gmail.com>
2020-11-22 19:07:46 -0500

    commit tree bc14c259f38602feeab38f075186b548e4a04d9e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+    counter = 0
+    collection = []
+    while counter < array.size
+        collection << yield(array[counter])
+        counter += 1
+    end
+    collection
+end
 
+#array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect(array) do |name|
+#  name.split(" ").first
+#end 
 
